3165816c34e2b7cd1eb1e342a7c8eef801f29042
anushakamran06 <anushakamran06@gmail.com>
2026-04-30 01:14:25 +0800

    add ask yourself why
    Implemented methods for adding, retrieving, and removing key-value pairs in MyHashMap.

--- Data Structures & Algorithms/design-hashmap/submission-0.py
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -1,3 +1,4 @@
+#MY code 
 class MyHashMap:
     def __init__(self):
         self.data = {}
@@ -17,10 +18,22 @@ class MyHashMap:
     def remove(self, key: int) -> None:
         if key in self.data:
             del self.data[key]
+#CORRECT
+class MyHashMap:
+    def __init__(self):
+        self.data = {}
 
+    def put(self, key: int, value: int) -> None:
+        self.data[key] = value
 
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+    def get(self, key: int) -> int:
+        if key in self.data:
+            return self.data[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.data:
+            del self.data[key]
+
+#need for self.data to create an empty dict PER INSTANCE. ask yourself why?
+#
