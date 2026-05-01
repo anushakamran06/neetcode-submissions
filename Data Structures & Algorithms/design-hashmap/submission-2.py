a16919a0e660f9be4dbd4ec9201fc6a56048b2af
anushakamran06 <anushakamran06@gmail.com>
2026-05-01 18:47:16 +0800

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.py
@@ -0,0 +1,18 @@
+class MyHashMap:
+    def __init__(self):
+        self.data = {}
+
+    def put(self, key: int, value: int) -> None:
+            self.data[key] = value
+    
+    def get(self, key: int) -> int:
+        if key in self.data:
+            return self.data[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.data:
+            del self.data[key]
+
+
+#need for self.data to create an empty dictionary per instance. ask urself why?
